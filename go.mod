a6c960ca9bbce6be9d373202f25faf54f2f2d422
andrefebrianto <andrefebrianto88@gmail.com>
2020-08-23 22:11:04 +0700

    Create main app module

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/andrefebrianto/rest-api
+
+go 1.13
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/spf13/viper v1.7.1
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/spf13/viper"
+)
+
+// Handler
+func healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "Service is running")
+}
+
+func setCorsHeader(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(context echo.Context) error {
+		context.Response().Header().Set("Access-Control-Allow-Origin", "*")
+		return next(context)
+	}
+}
+
+func loadConfig() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	loadConfig()
+
+	// Echo instance
+	httpServer := echo.New()
+
+	// Middleware
+	httpServer.Use(middleware.Logger())
+	httpServer.Use(middleware.Recover())
+	httpServer.Use(setCorsHeader)
+
+	// Routes
+	httpServer.GET("/", healthCheck)
+
+	// Start server
+	httpServer.Logger.Fatal(httpServer.Start(viper.GetString("server.address")))
+}
